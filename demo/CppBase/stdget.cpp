cee8e7ea193401bd97cd63856e27b89064259f14
playbar <hgl868@126.com>
2018-02-26 11:53:27 +0800

    添加unique_resource 测试

---
+++ demo/CppBase/stdget.cpp
@@ -0,0 +1,26 @@
+//
+// Created by hou guoli on 2018/2/24.
+//
+
+#include <iostream>
+#include <string>
+#include <tuple>
+using namespace std;
+
+int main()
+{
+//    foo<int, double> f;
+//    get<0>( f ) = 7;
+//    get<1>( f ) = 3.14;
+//    std::cout << get<0>(f) << "," << get<1>(f) << "\n";
+//
+//    auto t = std::make_tuple(1, "Foo", 3.14);
+//    // index-based access
+//    std::cout << "(" << std::get<0>(t) << ", " << std::get<1>(t)
+//              << ", " << std::get<2>(t) << ")\n";
+//    // type-based access
+//    std::cout << "(" << std::get<int>(t) << ", " << std::get<const char*>(t)
+//              << ", " << std::get<double>(t) << ")\n";
+    // Note: std::tie and structured binding may also be used to decompose a tuple
+}
+
---
+++ unique_resource/test1.cpp
@@ -0,0 +1,45 @@
+//
+// Created by hou guoli on 2018/2/26.
+//
+
+#include <iostream>
+#include "unique_resource.hpp"
+
+// some dummy resource functions
+using handle_t = int;
+decltype(auto) GenHandle(handle_t i)
+{
+    std::cout<< "get handle " << i << std::endl;
+    return i;
+}
+decltype(auto) ReleaseHandle(handle_t handle) {
+    std::cout << "release handle " << handle << std::endl;
+}
+
+decltype(auto) make_unique_handle(handle_t i) {
+    return std_experimental::make_unique_resource(
+            GenHandle(i), &ReleaseHandle);
+}
+int main() {
+    auto handle1 = make_unique_handle(1);
+    {
+        auto handle2 = make_unique_handle(2);
+        std::cout << handle2.get() << std::endl; // raw handle access by get()
+        // here, handle2 is released.
+    }
+    auto handle3 = make_unique_handle(3);
+    handle3.reset(); // release handle explicitly
+    {
+        auto handle4 = make_unique_handle(4);
+        auto raw_handle4 = handle4.release(); // strip RAII wrapper
+        ReleaseHandle(raw_handle4); // now, we should release handle by hand.
+    }
+    {
+        auto handle5 = make_unique_handle(5);
+        auto handle6 = std::move(handle5); // move construct
+        handle6.reset(GenHandle(6)); // reset raw handle
+    }
+
+    std::cout << "Bye!" << std::endl;
+}
+

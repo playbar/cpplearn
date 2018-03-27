6f603e99e2d2391cec7fcf0ef66f47e7271bbb7f
playbar <hgl868@126.com>
2018-03-27 18:38:25 +0800

    modify code

--- template/basics/max.cpp
+++ template/basics/max.cpp
@@ -12,24 +12,10 @@
 #include <string>
 #include "max.hpp"
 
-template <typename T>
-T const& min(T const& a, T const& b)
-{
-    return ( (a < b) ? a : b );
-}
-
-template <typename T1, typename T2>
-T1 const& mymin(T1 const& a, T2 const& b)
-{
-    return ( (a < b) ? a : b );
-}
-
 int main()
 {
     int i = 42;
     std::cout << "max(7,i):   " << ::max(7,i) << std::endl;
-    std::cout << "min(7,i):   " << ::min(7, i) << std::endl;
-    std::cout << "mymin(7,i):   " << ::mymin(7, i) << std::endl;
 
     double f1 = 3.4;
     double f2 = -6.7;
@@ -38,6 +24,4 @@ int main()
     std::string s1 = "mathematics";
     std::string s2 = "math";
     std::cout << "max(s1,s2): " << ::max(s1,s2) << std::endl;
-
-    std::cout << "max(7,f1): " << ::max<float>(7, f1) << std::endl;
 }

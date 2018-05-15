b5f50852befe9247ec3bd8aa8403222e939938e6
playbar <hgl868@126.com>
2018-05-15 14:52:50 +0800

    添加测试代码

--- demo/CppBase/unique_ptr.cpp
+++ demo/CppBase/unique_ptr.cpp
@@ -13,9 +13,17 @@
 // reference: http://en.cppreference.com/w/cpp/memory/unique_ptr
 struct Foo
 {
-	Foo()      { std::cout << "Foo::Foo\n"; }
-	~Foo()     { std::cout << "Foo::~Foo\n"; }
-	void bar() { std::cout << "Foo::bar\n"; }
+	Foo() {
+		data = 5;
+		std::cout << "Foo::Foo\n";
+	}
+	~Foo() {
+		std::cout << "Foo::~Foo\n";
+	}
+	void bar() {
+		std::cout << "Foo::bar " << data << " \n";
+	}
+	int data;
 };
 
 void f(const Foo &)
@@ -26,7 +34,9 @@ void f(const Foo &)
 int test_unique_ptr1()
 {
 	std::unique_ptr<Foo> p1(new Foo);  // p1 owns Foo
-	if (p1) p1->bar();
+	if (p1)
+		p1->bar();
+	p1 = NULL;
 
 	{
 		std::unique_ptr<Foo> p2(std::move(p1));  // now p2 owns Foo
@@ -37,7 +47,9 @@ int test_unique_ptr1()
 		std::cout << "destroying p2...\n";
 	}
 
-	if (p1) p1->bar();
+	if (p1)
+		p1->bar();
+
 
 	// Foo instance is destroyed when p1 goes out of scope
 
--- demo/CppBase/vector.hpp
+++ demo/CppBase/vector.hpp
@@ -6,4 +6,27 @@ int test_vector_access();
 int test_vector_operation();
 int test_vector_two_dimension();
 
+class Vec5f
+{
+public:
+    Vec5f(){
+        std::cout<<"vec5f construct default" << std::endl;
+        a = 0;
+        b = 0;
+        c = 0;
+        d = 0;
+        e = 0;
+    }
+    Vec5f(float _a, float _b, float _c, float _d, float _e)
+            :a(_a), b(_b), c(_c), d(_d), e(_e){
+        std::cout<<"vec5f construct" << std::endl;
+    }
+    float a;
+    float b;
+    float c;
+    float d;
+    float e;
+};
+
+
 #endif // FBC_MESSY_TEST_VECTOR_HPP

1715a341b58bbc71716dc8c8a7b0ee2aaf2c4064
hougl <hgl868@126.com>
2019-10-22 19:30:08 +0800

    modify code

--- demo/DesignPatterns/TemplatePattern/main.cpp
+++ demo/DesignPatterns/TemplatePattern/main.cpp
@@ -1,5 +1,82 @@
-#include "Tea.hpp"
-#include "Coffee.hpp"
+#include <iostream>
+
+class CaffeineBeverage {
+public:
+    virtual ~CaffeineBeverage(){}
+
+    void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        if(customerWantsCondiments())
+            addCondiments();
+    }
+
+    virtual void brew() = 0;
+    virtual void addCondiments() = 0;
+
+    void boilWater() {
+        std::cout << "Boiling water\n";
+    }
+
+    void pourInCup() {
+        std::cout << "Pouring into cup\n";
+    }
+
+    virtual bool customerWantsCondiments() {
+        return true;
+    }
+};
+
+class Tea : public CaffeineBeverage {
+public:
+    ~Tea(){}
+
+    void brew() {
+        std::cout << "Steeping the tea\n";
+    }
+
+    void addCondiments() {
+        std::cout << "Adding Lemon\n";
+    }
+
+    bool customerWantsCondiments()
+    {
+        char c;
+        std::cout << "Do you want to add condiments?\n";
+        std::cin >> c;
+
+        if (c == 'y')
+            return true;
+        else
+            return false;
+    }
+};
+
+class Coffee : public CaffeineBeverage {
+public:
+    ~Coffee(){}
+
+    void brew() {
+        std::cout << "Dripping Coffee through filter\n";
+    }
+
+    void addCondiments() {
+        std::cout << "Adding Sugar and Milk\n";
+    }
+
+    bool customerWantsCondiments() {
+        char c;
+        std::cout << "Do you want to add condiments?\n";
+        std::cin >> c;
+
+        if(c == 'y')
+            return true;
+        else
+            return false;
+    }
+};
+
 
 int main() {
     Tea* tea = new Tea();

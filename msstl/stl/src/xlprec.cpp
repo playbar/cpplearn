d8d1dc9c15b3af4fa351aa1d01abaa3c11142596
hougl <hgl868@126.com>
2019-12-02 14:48:24 +0800

    mv msstl

--- msstl/stl/src/xlprec.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-// _LXp* functions
-
-#include "xmath.h"
-#include "xxlftype.h"
-#include "xxxprec.h"
--- msstl/stl/src/xprec.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-// _Xp* functions
-
-#include "xmath.h"
-#include "xxdftype.h"
-#include "xxxprec.h"

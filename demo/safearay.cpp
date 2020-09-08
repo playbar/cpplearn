5e1da6683e09e733bc24b705d16b596f4be2d9ca
playbar <hgl868@126.com>
2020-09-09 07:50:37 +0800

    add safearray

---
+++ demo/safearay.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+const int SIZE = 10;
+
+class safearay
+{
+private:
+    int arr[SIZE];
+public:
+    safearay()
+    {
+        register int i;
+        for(i = 0; i < SIZE; i++)
+        {
+            arr[i] = i;
+        }
+    }
+    int& operator[](int i)
+    {
+        if( i > SIZE )
+        {
+            cout << "索引超过最大值" <<endl;
+            // 返回第一个元素
+            return arr[0];
+        }
+        return arr[i];
+    }
+};
+
+int main()
+{
+    safearay A;
+
+    cout << "A[2] 的值为 : " << A[2] <<endl;
+    cout << "A[5] 的值为 : " << A[5]<<endl;
+    cout << "A[12] 的值为 : " << A[12]<<endl;
+
+    return 0;
+}
+

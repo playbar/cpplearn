22dec348e0653d696ea375025370206e7f46e9cf
hougl <>
2020-11-21 12:16:01 +0800

    add xls lib

---
+++ xlslib/targets/buildMD5.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+./testC /Volumes/Data/svn/xlslib/xlslib/targets/test
+./testCPP /Volumes/Data/svn/xlslib/xlslib/targets/test
+./PR2859188 /Volumes/Data/svn/xlslib/xlslib/targets/test
+./PR3076678 /Volumes/Data/svn/xlslib/xlslib/targets/test
+
+
+
+
